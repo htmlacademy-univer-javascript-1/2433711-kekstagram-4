094ea1d9858697027a7c32caed936296dbd112eb
Occideree <incorporeall@yandex.com>
2023-12-15 14:45:50 +0500

    Масштабирование и стили фотографии

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,82 +1,46 @@
 import { startBodyMovement, stopBodyMovement } from './util.js';
+import { pristine } from './validate-form.js';
+import { withUploadOpening, withUploadClosing } from './scale-control.js';
+import { deleteSlider } from './slider.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const fileInput = document.querySelector('#upload-file');
-const imageEditor = document.querySelector('.img-upload__overlay');
-const imageEditorPreview = document.querySelector('img.img-upload__preview');
+const imageModal = document.querySelector('.img-upload__overlay');
+const imageModalPreview = document.querySelector('img.img-upload__preview');
 const editorCloser = document.querySelector('.img-upload__cancel');
-const pristine = new Pristine(uploadForm, {
-  classTo: 'img-upload__field-wrapper',
-  errorClass: 'form__item--invalid',
-  successClass: 'form__item--valid',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextTag: 'span',
-  errorTextClass: 'hasgtag__error',
-});
 const hashtagInput = document.querySelector('.text__hashtags');
-const hashtagTemplate = /#[a-zа-яё0-9]{1,19}$/i;
-const MAX_HASHTAGS = 5;
-let hashtagErrorMessage;
+
 const descriptionInput = document.querySelector('.text__description');
 
 const onDocumentKeydown = (evt) => {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    hideEditor();
+    hideModal();
   }
 };
 
-const hideEditor = () => {
+const hideModal = () => {
   if (
     hashtagInput !== document.activeElement &&
     descriptionInput !== document.activeElement
   ) {
-    imageEditor.classList.add('hidden');
+    imageModal.classList.add('hidden');
     document.removeEventListener('keydown', onDocumentKeydown);
-    editorCloser.removeEventListener('click', hideEditor);
+    editorCloser.removeEventListener('click', hideModal);
     startBodyMovement();
+    withUploadClosing();
+    deleteSlider();
     uploadForm.reset();
   }
 };
 fileInput.addEventListener('change', () => {
-  imageEditor.classList.remove('hidden');
-  imageEditorPreview.src = URL.createObjectURL(fileInput.files[0]);
+  imageModal.classList.remove('hidden');
+  imageModalPreview.src = URL.createObjectURL(fileInput.files[0]);
   stopBodyMovement();
   document.addEventListener('keydown', onDocumentKeydown);
-  editorCloser.addEventListener('click', hideEditor);
+  editorCloser.addEventListener('click', hideModal);
+  withUploadOpening();
 });
-
-const isHashtagValid = () => {
-  const hashtagCounter = hashtagInput.value
-    .split(' ')
-    .map((hashtag) => hashtag.toLowerCase());
-  for (let hashtag of hashtagCounter) {
-    hashtag = hashtag.toLowerCase();
-    if (
-      hashtag.split('#').length > 2 ||
-      (!hashtagTemplate.test(hashtag) && hashtag !== '')
-    ) {
-      hashtagErrorMessage = 'Введён невалидный хэш-тег';
-      return false;
-    } else if (hashtagCounter.length > MAX_HASHTAGS) {
-      hashtagErrorMessage = 'Превышено количество хэш-тегов';
-      return false;
-    }
-  }
-  if (new Set(hashtagCounter).size !== hashtagCounter.length) {
-    hashtagErrorMessage = 'Хэш-теги повторяются.';
-    return false;
-  }
-  return true;
-};
-const createErrorMessage = () => hashtagErrorMessage;
-pristine.addValidator(
-  hashtagInput,
-  isHashtagValid,
-  createErrorMessage,
-  1,
-  false
-);
 
 uploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
