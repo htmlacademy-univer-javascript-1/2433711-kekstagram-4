f09e6804f1a3b3bb4745c6f0bf3cbbc3a21c2daa
Keks <keks@htmlacademy.ru>
2023-12-22 14:53:38 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/filters.js
@@ -0,0 +1,44 @@
+import { getRandomInt } from './util.js';
+const defaultFilter = document.querySelector('#filter-default');
+const randomFilter = document.querySelector('#filter-random');
+const discussedFilter = document.querySelector('#filter-discussed');
+
+export const sortByCommentsAmount = (photoA, photoB) => {
+  const commentsCountB = photoB.comments.length;
+  const commentsCountA = photoA.comments.length;
+  return commentsCountB - commentsCountA;
+};
+export const randomizePhotos = (photoA, photoB) => {
+  const photoARandomValue = getRandomInt(0, 100);
+  const photoBRandomValue = getRandomInt(0, 100);
+  return photoBRandomValue - photoARandomValue;
+};
+export const defaultSort = (photoA, photoB) => 0;
+
+/*const defaultFilterListener = (cb) =>{
+  defaultFilter.addEventListener('click',)
+}*/
+export const setDiscussedFilter = (cb) => {
+  discussedFilter.addEventListener('click', () => {
+    randomFilter.classList.remove('img-filters__button--active');
+    defaultFilter.classList.remove('img-filters__button--active');
+    discussedFilter.classList.add('img-filters__button--active');
+    cb();
+  });
+};
+export const setDefaultFilter = (cb) => {
+  defaultFilter.addEventListener('click', () => {
+    randomFilter.classList.remove('img-filters__button--active');
+    defaultFilter.classList.add('img-filters__button--active');
+    discussedFilter.classList.remove('img-filters__button--active');
+    cb();
+  });
+};
+export const setRandomFilter = (cb) => {
+  randomFilter.addEventListener('click', () => {
+    randomFilter.classList.add('img-filters__button--active');
+    defaultFilter.classList.remove('img-filters__button--active');
+    discussedFilter.classList.remove('img-filters__button--active');
+    cb();
+  });
+};
---
+++ 13/js/main.js
@@ -0,0 +1,32 @@
+import { createMiniatures } from './miniatures.js';
+import './big-picture.js';
+import './upload-form.js';
+import './slider.js';
+import { getData } from './api.js';
+import { showAlert, debounce } from './util.js';
+import {
+  setDiscussedFilter,
+  sortByCommentsAmount,
+  setDefaultFilter,
+  setRandomFilter,
+  defaultSort,
+  randomizePhotos,
+} from './filters.js';
+
+getData()
+  .then((photos) => {
+    createMiniatures(photos);
+    setDiscussedFilter(
+      debounce(() => createMiniatures(photos, sortByCommentsAmount))
+    );
+    setRandomFilter(debounce(() => createMiniatures(photos, randomizePhotos)));
+    setDefaultFilter(debounce(() => createMiniatures(photos, defaultSort)));
+  })
+  .then(
+    document
+      .querySelector('.img-filters')
+      .classList.remove('img-filters--inactive')
+  )
+  .catch((error) => {
+    showAlert(error);
+  });
